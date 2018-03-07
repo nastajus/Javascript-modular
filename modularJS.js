7906f9e796f06b43d155cd412fbf059afabdb326
Ian Nastajus <ian.nastajus@gmail.com>
2018-03-06 19:31:00 -0500

    revealing module pattern conversion completed. removed all the <this>'s, stored people in IIFE, returned only API parts cared about addPerson & deletePerson, but not render.

--- modularJS.js
+++ modularJS.js
@@ -1,50 +1,47 @@
-(function() {
+// Revealing Module Pattern 
+// this is a more traditional vanilla javascript style. 
+// the init functionality gets triggered automatically, it doesn't need to be a separate init() method anymore, as this is an IIFE
 
-	var people = {
-		people: ['Will', 'Laura'],
-		init: function() {
-			//efficient DOM usage
-			this.cacheDom();
-			this.bindEvents();
-			this.render();
-		},
-		cacheDom: function() {
-			this.$el = $('#peopleModule');	//author's convention: likes to add $ to vars from JQuery searches. 
-			this.$button = this.$el.find('button');
-			this.$input = this.$el.find('input');
-			this.$ul = this.$el.find('ul');
-			this.template = this.$el.find('#people-template').html();
-
-		},
-		bindEvents: function() {
-			//usually don't want to write functions directly in an event bind, it's just not a good pattern. for a lot of reasons.
-			this.$button.on('click', this.addPerson.bind(this));
-			this.$ul.delegate('em.del', 'click', this.deletePerson.bind(this));
-		},
-		//this is the guy (function) that touches HTML, not anywhere else
-		render: function() {
-			var data = {
-				people: this.people,
-			};
-			this.$ul.html(Mustache.render(this.template, data)); 
-		},
-		addPerson: function() {
-			this.people.push(this.$input.val());
-			this.render();
-			this.$input.val('');
-		},
-		deletePerson: function(e) {
-			var $remove = $(e.target).closest('li');
-			var i = this.$ul.find('li').index($remove);
-			
-			this.people.splice(i,1);
-			this.render();
-		}
-	};
+var people = (function() {
+	var people = ['Will', 'Laura'];
 	
-	people.init();
+	//cache COM
+	var $el = $('#peopleModule');
+	var $button = $el.find('button');
+	var $input = $el.find('input');
+	var $ul = $el.find('ul');
+	var template = $el.find('#people-template').html();
 
-})()
+	//bind events
+	$button.on('click', addPerson);
+	$ul.delegate('em.del', 'click', deletePerson);
+
+	render();
+	
+	function render() {
+		$ul.html(Mustache.render(template, {people: people})); 
+	}
+	
+	function addPerson() {
+		people.push($input.val());
+		render();
+		$input.val('');
+	}
+	
+	function deletePerson(e) {
+		var $remove = $(e.target).closest('li');
+		var i = $ul.find('li').index($remove);
+		
+		people.splice(i,1);
+		render();
+	}
+	
+	return {
+		addPerson: addPerson,
+		deletePerson: deletePerson
+	};
+})();
+	
 
 //object literal
 var myModule = {
